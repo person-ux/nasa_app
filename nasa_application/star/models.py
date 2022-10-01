7e131eb9fe1fa9698ae25df35d190d3d251222c5
Rohan Yadav <yadavrohan120@gmail.com>
2022-10-01 14:04:28 +0530

    First commit

---
+++ nasa_application/star/models.py
@@ -0,0 +1,36 @@
+from ctypes.wintypes import tagMSG
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from taggit.managers import TaggableManager
+from taggit.models import Tag
+
+# Create your models here.
+
+
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = RichTextField(null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    tag=TaggableManager()
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+class TagMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(TagMixin, self).get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
