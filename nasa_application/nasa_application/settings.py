5836d0941ce25c5a4af186bc195dbad8ea78ed3f
Rohan Yadav <yadavrohan120@gmail.com>
2022-10-02 20:40:13 +0530

    additional files and changes for Heroku

--- nasa_application/nasa_application/settings.py
+++ nasa_application/nasa_application/settings.py
@@ -23,14 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jsqq=yhx0$xcvmqp_@plc&r=wt6ufu*dppxmi*1^-jv*#&92o1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +135,6 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 TAGGIT_CASE_INSENSITIVE = True
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+WHITENOISE_USE_FINDERS = True
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
